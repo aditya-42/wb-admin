71fc8a64b43f98713d371df6e04a03368cec922d
Aditya Purohit <aditya.s.purohit@gmail.com>
2025-06-27 02:50:15 -0400

    profile_fix

--- src/components/DashboardClientWrapper.tsx
+++ src/components/DashboardClientWrapper.tsx
@@ -2,19 +2,25 @@
 
 import { useSearchParams } from "next/navigation";
 import DashboardClient from "@/components/DashboardClient";
+import ProfileView from "./ProfileView";
 
 export default function DashboardClientWrapper({
   data,
+  admin,
 }: {
   data: {
     all: { approved: any[]; unapproved: any[] };
     business: { approved: any[]; unapproved: any[] };
     individual: { approved: any[]; unapproved: any[] };
   };
+  admin: any;
 }) {
   const searchParams = useSearchParams();
   const tab = searchParams.get("tab") ?? "all";
 
+  if (tab === "profile") {
+    return <ProfileView admin={admin} />;
+  }
   const filteredData =
     tab === "business"
       ? data.business
